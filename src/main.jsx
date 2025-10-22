4aa8e59b41db75b869bca3c6289d5b0e616840e8
Enzzzzz <contatoenzz@gmail.com>
2025-10-22 18:04:13 -0300

    added a new page and fixed logo

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import Home from './routes/Home'
+import Page2 from './routes/Page2'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />
+  },
+  {
+    path: "/page2",
+    element: <Page2 />
   }
 ]);
 
--- src/routes/Page2.jsx
+++ src/routes/Page2.jsx
@@ -2,20 +2,20 @@ import Navbar from "../components/navbar"
 import Logo from "/Logo2-Sem-Fundo.png"
 import Logo2 from "/Logo-Sem-Fundo.png"
 
-function Home() {
+function Page2() {
   return (
     <div className="">
       <Navbar />
         <div id="firstLayer" className="section bg-blue-gradient flex flex-col">
-          <img src={Logo} className="mt-24 sm:-mt-32 w-64 sm:w-96 md:w-[450px] 2xlg:w-[550px]" />
-          <h1 className="text-3xl text-center m-10 font-extrabold text-text sm:text-4xl lg:text-5xl xl:text-7xl">Template Tailwind and ReactJS</h1>
+          <img src={Logo} className="mt-24 sm:-mt-32 w-64 sm:w-96 md:w-[450px] 2xlg:w-[550px]"/>
+          <h1 className="text-3xl text-center m-10 font-extrabold text-text sm:text-4xl lg:text-5xl xl:text-7xl">This is the second page</h1>
         </div>
         <div id="secondLayer" className="section bg-red-gradient-reverse">
           <img src={Logo2} className="mt-24 w-64 sm:w-96 md:w-[450px] 2xlg:w-[550px]" />
-          <h1 className="text-3xl text-center m-10 font-extrabold text-tertiary sm:text-4xl lg:text-5xl xl:text-7xl">Template Tailwind and ReactJS</h1>
+          <h1 className="text-3xl text-center m-10 font-extrabold text-tertiary sm:text-4xl lg:text-5xl xl:text-7xl">This is the second page</h1>
         </div>
     </div>
   )
 }
 
-export default Home
+export default Page2
